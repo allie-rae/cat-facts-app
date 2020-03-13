2725798c0d596e014cd6f72c8480683bdd14ea4a
Allie Robinson <allierob93@gmail.com>
2020-03-13 03:20:21 -0500

    added styled-components, fixed some styling issues

--- React-Redux-App/src/App.js
+++ React-Redux-App/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import Nav1 from './components/Nav';
 import './App.css';
 import GenerateFact from './components/GenerateFact';
+import styled from 'styled-components';
+
+const StyledH1 = styled.h1`
+font-family: 'indie flower';
+margin: 2%;
+`
 
 function App() {
   return (
     <div className="App">
       <Nav1 />
-      <h1>Cat Facts App</h1>
+      <StyledH1>Cat Facts App</StyledH1>
       <GenerateFact />
     </div>
   );
--- React-Redux-App/src/components/GenerateFact.js
+++ React-Redux-App/src/components/GenerateFact.js
@@ -1,43 +1,70 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { getCatFact } from '../actions';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Spinner from 'react-bootstrap/Spinner';
-import '../App.css';
+import React from "react";
+import { connect } from "react-redux";
+import { getCatFact } from "../actions";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Spinner from "react-bootstrap/Spinner";
+import "../App.css";
+import styled from "styled-components";
 
-const GenerateFact = (props) => {
-    console.log("generateFact props", props)
-    return <>
-        <div className="wrapper">
-            {props.isFetching && <><Spinner animation="border" /> <br /> <br /></>}
-            {!props.isFetching && props.catFact &&
-                <Card bg="secondary" text="white" style={{ width: '18rem' }} className="center">
-                    <Card.Header><i class="fas fa-cat"></i></Card.Header>
-                    <Card.Body>
-                        <Card.Title>Random Cat Fact:</Card.Title>
-                        <Card.Text>
-                            {props.catFact}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>}
-            <Button onClick={(e) => {
-                e.preventDefault();
-                props.getCatFact()
-            }}
-                variant="outline-dark">
-                Generate Fact
+const StyledH2 = styled.h2`
+  font-family: "indie flower";
+  margin: 2%;
+`;
+const StyledP = styled.p`
+  font-family: "indie flower";
+`;
+
+const StyledDiv = styled.div`
+  font-family: "indie flower";
+`;
+
+const GenerateFact = props => {
+  console.log("generateFact props", props);
+  return (
+    <>
+      <div className="wrapper">
+        {props.isFetching && (
+          <>
+            <Spinner animation="border" /> <br /> <br />
+          </>
+        )}
+        {!props.isFetching && props.catFact && (
+          <Card
+            bg="dark"
+            text="white"
+            style={{ width: "18rem" }}
+            className="center"
+          >
+            <Card.Header>
+              <i class="fas fa-cat"></i>
+              <StyledH2>Random Cat Fact:</StyledH2>
+            </Card.Header>
+            <Card.Body>
+              <StyledP>{props.catFact}</StyledP>
+            </Card.Body>
+          </Card>
+        )}
+        <Button
+          onClick={e => {
+            e.preventDefault();
+            props.getCatFact();
+          }}
+          variant="outline-dark"
+        >
+          <StyledDiv>Generate Fact</StyledDiv>
         </Button>
-        </div>
+      </div>
     </>
-}
+  );
+};
 
 const mapStateToProps = state => {
-    return {
-        catFact: state.catFact,
-        isFetching: state.isFetching,
-        error: state.error
-    }
-}
+  return {
+    catFact: state.catFact,
+    isFetching: state.isFetching,
+    error: state.error
+  };
+};
 
 export default connect(mapStateToProps, { getCatFact })(GenerateFact);
